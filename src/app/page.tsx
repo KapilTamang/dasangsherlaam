a22977235e787d46d553f0479b061ae9868b6931
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-06-26 19:21:57 +0545

    fix: thumbnail card compenent mapping fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,49 @@
+import Card from '@/components/shared/card';
 import FeaturedCard from '@/components/shared/featured-card';
 import SectionTitle from '@/components/shared/section-title';
 import ThumbnailCard from '@/components/shared/thumbnail-card';
+import blogs from '@/data/blogs';
 
 export default function Home() {
-  return (
-    <main>
-        <section className="section-banner section-base-style bg-accent">
-			<div className="banner-conatiner container-base-style">
-				<div className="banner-content grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-6">
-					<div className="banner-featured cold-span-1 lg:col-span-2 flex flex-col gap-3 md:gap-4">
-						<SectionTitle title="featured"/>
-						<FeaturedCard/>
-					</div>
-					<div className="banner-trending col-span-1 flex flex-col gap-3 md:gap-4">
-						<SectionTitle title="trending"/>
-						<div className="banner-trending-content flex flex-col gap-8 md:gap-3">
-							<ThumbnailCard/>
+
+	//Fetching dummy data..
+	const dummyBlogs = blogs;
+	const trending = dummyBlogs.filter((blog) => blog.category !== 'featured').slice(0,6);
+	//Finding featured blog
+	const featured = dummyBlogs.find((blog) => blog.category == 'featured');
+
+	return (
+		<main>
+			<section className="section-banner section-base-style bg-accent">
+				<div className="banner-conatiner container-base-style">
+					<div className="banner-content grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-6">
+						<div className="banner-featured cold-span-1 lg:col-span-2 flex flex-col gap-3 md:gap-4">
+							<SectionTitle title="featured"/>
+							<FeaturedCard/>
+						</div>
+						<div className="banner-trending col-span-1 flex flex-col gap-3 md:gap-4">
+							<SectionTitle title="trending"/>
+							<div className="banner-trending-content flex flex-col gap-8 md:gap-3">
+								{
+									trending.map((blog) =>(
+										<ThumbnailCard key={blog.id} data={blog}/>
+									))
+								}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-        </section>
-		<section className="section-todays-pick section-base-style">
-			<div className="todays-pick-container container-base-style">
-				<div className="todays-pick-content flex flex-col gap-3 md:gap-4">
-					<SectionTitle title="today's pick"/>
-					sadfasdf
+			</section>
+			<section className="section-todays-pick section-base-style">
+				<div className="todays-pick-container container-base-style">
+					<div className="todays-pick-content flex flex-col gap-3 md:gap-4">
+						<SectionTitle title="today's pick"/>
+						<div className="todays-pick-card-conatiner">
+							<Card/>
+						</div>
+					</div>
 				</div>
-			</div>
-		</section>
-    </main>
-  );
+			</section>
+		</main>
+	);
 }
--- src/components/shared/thumbnail-card.tsx
+++ src/components/shared/thumbnail-card.tsx
@@ -1,43 +1,47 @@
 import Link  from 'next/link';
-import { CalendarClock, UserPen } from 'lucide-react';
 import CategoryTag from './category-tag';
-import blogs from '@/data/blogs';
 
-export default function ThumbnailCard() {
-    const dummyBlogs = blogs;
-    const trending = dummyBlogs.filter((blog) => blog.category !== 'featured').slice(0,6);
+interface cardItem {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    category: string;
+    imageURL: string;
+    date: string;
+    author: string; 
+}
+
+interface cardItemProps {
+    data: cardItem
+}
+
+export default function ThumbnailCard({data} : cardItemProps) {
     return(
-        <>
-            {
-                trending.map((blog, index) => (
-                    <Link key={index} className="group pb-0 md:pb-4 md:border-b md:border-foreground/20" href={`/blog/${blog.slug}`}>
-                        <article className="card flex gap-0 md:gap-3 overflow-hidden">
-                            <div className="card-image bg-primary flex-1">
-                                <img className="md:w-full md:h-full object-cover group-hover:scale-110 duration-500 ease-in-out" src={blog?.imageURL ?? ''} alt="thumbnail-image" />
-                            </div>
-                            <div className="card-body flex flex-2 md:flex-3 flex-col items-start gap-2 md:gap-0 bg-primary md:bg-transparent text-card-featured-foreground md:text-card-thumbnail-foreground px-2 pb-2 md:p-0">
-                                <div className="card-header text-xl font-bold">
-                                    <div className="category-tag block md:hidden">
-                                        <CategoryTag title={blog.category}/>
-                                    </div>
-                                    <h5 className="md:line-clamp-1 underline md:no-underline md:group-hover:underline text-[1.2rem] capitalize duration-300">
-                                       {blog.title}
-                                    </h5>
-                                </div>
-                                <div className="card-footer flex flex-col gap-1 md:text-muted-foreground text-[1rem]">
-                                     <div className="card-footer-date">
-                                        {blog.author}
-                                    </div>
-                                    <div className="card-footer-date">
-                                        {blog.date}
-                                    </div>
-                                </div>
-                            </div>
-                        </article>
-                    </Link>
-                ))
-            }
-        </>
-       
+        <Link className="group pb-0 md:pb-4 md:border-b md:border-foreground/20" href={`/blog/${data.slug}`}>
+            <article className="card flex gap-0 md:gap-3 overflow-hidden">
+                <div className="card-image bg-primary md:bg-transparent flex-1">
+                    <img className="md:w-full md:h-full object-cover group-hover:scale-110 duration-500 ease-in-out" src={data.imageURL ?? ''} alt="thumbnail-image" />
+                </div>
+                <div className="card-body flex flex-2 md:flex-3 flex-col items-start gap-2 md:gap-0 bg-primary md:bg-transparent text-card-featured-foreground md:text-card-thumbnail-foreground px-2 pb-2 md:p-0">
+                    <div className="card-header text-xl font-bold">
+                        <div className="category-tag block md:hidden">
+                            <CategoryTag title={data.category}/>
+                        </div>
+                        <h5 className="md:line-clamp-1 underline md:no-underline md:group-hover:underline text-[1.2rem] capitalize duration-300">
+                            {data.title}
+                        </h5>
+                    </div>
+                    <div className="card-footer flex flex-col gap-1 md:text-muted-foreground text-[1rem] md:text-[0.9rem]">
+                            <div className="card-footer-date">
+                            {data.author}
+                        </div>
+                        <div className="card-footer-date">
+                            {data.date}
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </Link>
     )
 }
