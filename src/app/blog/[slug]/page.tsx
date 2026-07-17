ce86b49d7985c21a67ec28774cec847ebb392115
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-07-17 15:21:23 +0545

    feat: image skeleton and single blog page content skeleton created

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -13,7 +13,8 @@ import { CalendarClock, UserPen } from 'lucide-react';
 import SectionTitle from "@/components/ui/section-title";
 import CardRow from "@/components/shared/card-row";
 import  Card from '@/components/shared/card-column';
-import NewsletterPromoCard from "@/components/shared/card-newsletter-promo"
+import NewsletterPromoCard from "@/components/shared/card-newsletter-promo";
+import { ImageSkeleton, SingleBlogSkeleton} from '@/components/shared/skeletons';
 1
 export default function Blog() {
     //Retrieve slug from URL
@@ -45,67 +46,84 @@ export default function Blog() {
                     (<section className="section-single-blog section-base-style relative">
                         <div className="section-single-blog-container container-base-style">
                             <div className="single-blog-content flex flex-col md:flex-row gap-4 lg:gap-6">
-                                <article className="sinlge-blog-content-left flex flex-col flex-3 gap-4 md:gap-2 relative">
-                                    <div className="single-blog-content-left-info flex flex-col md:flex-row gap-4 md:gap-6 lg:gap-12 items-start md:items-center justify-between">
-                                        <div className="single-blog-content-left-info-img relative block md:hidden">
-                                            <div className="single-blog-content-left-img-conatiner">
-                                                <Image
-                                                className="shadow-lg" 
-                                                src={currentBlog.imageURL} 
-                                                alt={currentBlog.title}
-                                                width={1200}
-                                                height={1000}
-                                                />
+                                {
+                                    isLoading ? (
+                                        <SingleBlogSkeleton/>
+                                    ): (
+                                        <article className="sinlge-blog-content-left flex flex-col flex-3 gap-4 md:gap-2 relative">
+                                            <div className="single-blog-content-left-info flex flex-col md:flex-row gap-4 md:gap-6 lg:gap-12 items-start md:items-center justify-between">
+                                                <div className="single-blog-content-left-info-img w-full relative block md:hidden">
+                                                    <div className="single-blog-content-left-img-conatiner">
+                                                        <Image
+                                                        className="shadow-lg" 
+                                                        src={currentBlog.imageURL} 
+                                                        alt={currentBlog.title}
+                                                        width={1200}
+                                                        height={1000}
+                                                        priority
+                                                        />
+                                                    </div>
+                                                    <div className="single-blog-content-left-img-tag absolute top-3 left-2">
+                                                        <CategoryTag title={currentBlog.category}/>
+                                                    </div>
+                                                </div>
+                                                <div className="single-blog-content-left-info-category hidden md:block">
+                                                    <CategoryTag title={currentBlog.category}/>
+                                                </div>
+                                                <div className="single-blog-content-left-info-author-date w-full md:w-auto flex gap-6 lg:gap-12 justify-between text-[0.9rem] md:text-[1rem] text-muted-foreground">
+                                                    <div className="flex gap-2 items-center">
+                                                        <UserPen className="inline" size={20}/>
+                                                        {currentBlog.author}
+                                                    </div>
+                                                    <div className="flex gap-2 items-center">
+                                                        <CalendarClock className="inline" size={20}/>
+                                                        {currentBlog.date}
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="single-blog-content-left-img-tag absolute top-3 left-2">
-                                                <CategoryTag title={currentBlog.category}/>
+                                            <div className="single-blog-content-left-title text-[1.1rem] md:text-[1.2rem] lg:text-[1.2rem] font-bold capitalize px-4 py-3 border-l-2 border-l-primary/50 bg-accent sticky top-15 md:top-20">
+                                                {currentBlog.title}
                                             </div>
-                                        </div>
-                                        <div className="single-blog-content-left-info-category hidden md:block">
-                                             <CategoryTag title={currentBlog.category}/>
-                                        </div>
-                                        <div className="single-blog-content-left-info-author-date w-full md:w-auto flex gap-6 lg:gap-12 justify-between text-[0.9rem] md:text-[1rem] text-muted-foreground">
-                                            <div className="flex gap-2 items-center">
-                                                <UserPen className="inline" size={20}/>
-                                                {currentBlog.author}
+                                            <div className="single-blog-content-left-description order-4 mt-0 md:mt-2 lg:mt-4">
+                                                <p>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                    {currentBlog.description}<br></br><br></br>
+                                                </p>
                                             </div>
-                                            <div className="flex gap-2 items-center">
-                                                <CalendarClock className="inline" size={20}/>
-                                                {currentBlog.date}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="single-blog-content-left-title text-[1.1rem] md:text-[1.2rem] lg:text-[1.2rem] font-bold capitalize px-4 py-3 border-l-2 border-l-primary/50 bg-accent sticky top-15 md:top-20">
-                                        {currentBlog.title}
-                                    </div>
-                                    <div className="single-blog-content-left-description order-4 mt-0 md:mt-2 lg:mt-4">
-                                        <p>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                            {currentBlog.description}<br></br><br></br>
-                                        </p>
-                                    </div>
-                                </article>
+                                        </article>
+                                    )
+                                }
+        
                                 <div className="single-blog-content-right flex flex-col flex-2 gap-6 sticky top-25 h-full">
-                                    <div className="single-blog-content-img relative hidden md:block">
-                                        <div className="single-blog-content-img-conatiner">
-                                            <Image
-                                            className="shadow-lg" 
-                                            src={currentBlog.imageURL} 
-                                            alt={currentBlog.title}
-                                            width={1200}
-                                            height={1000}
-                                            />
-                                        </div>
-                                         <div className="single-blog-content-img-tag absolute top-3 left-2 block md:hidden">
-                                        <CategoryTag title={currentBlog.category}/>
-                                        </div>
+                                    <div className="single-blog-content-img w-full relative hidden md:block">
+                                        {
+                                            isLoading ? (
+                                                <ImageSkeleton/>
+                                            ): (
+                                                <>
+                                                    <div className="single-blog-content-img-conatiner">
+                                                        <Image
+                                                        className="shadow-lg" 
+                                                        src={currentBlog.imageURL} 
+                                                        alt={currentBlog.title}
+                                                        width={1200}
+                                                        height={1000}
+                                                        priority
+                                                        />
+                                                    </div>
+                                                    <div className="single-blog-content-img-tag absolute top-3 left-2 block md:hidden">
+                                                    <CategoryTag title={currentBlog.category}/>
+                                                    </div>
+                                                </>
+                                            )
+                                        }
                                     </div>
                                     <div className="single-blog-content-recommendation w-full flex flex-col gap-4 md:gap-2">
                                         <SectionTitle title="trending now"/>
@@ -128,7 +146,7 @@ export default function Blog() {
                     </section>)
             }
             <section className="section-similar section-base-style">
-                <div className="section-similar-container container-base-style flex flex-col gap-2 md:gap-6">
+                <div className="section-similar-container container-base-style flex flex-col gap-4 md:gap-6">
                     <div className="section-similar-title">
                         <SectionTitle title="similar blogs"/>
                     </div>
--- src/components/shared/skeletons.tsx
+++ src/components/shared/skeletons.tsx
@@ -18,7 +18,7 @@ export function CardFeaturedSkeleton() {
 export function CardRowSkeleton({type} : cardProps) {
     return (
         <CardContent className={`flex gap-2 w-full h-[110px] py-0 md:py-2 first-of-type:border-t-0 last-of-type:border-b-0 md:border-b md:border-foreground/20 ${type === 'exclusive' ? 'border-none' : ''}`}>
-            <Skeleton className="flex-1 h-[80%] rounded-none"/>
+            <Skeleton className="flex-1 h-full rounded-none"/>
             <CardDescription className="w-full h-full flex flex-2 flex-col gap-2 justify-start pt-4">
                 <Skeleton className="w-full h-2 rounded-none"/>
                 <Skeleton className="w-2/3 h-2 rounded-none"/>
@@ -71,3 +71,27 @@ export function CardColumnSkeleton({width}: cardProps) {
         </Card>
     )
 } 
+
+// Image Skeleton
+export function ImageSkeleton() {
+    return(
+        <Skeleton className="bg-primary h-[260px] sm:h-[400px] md:h-[260px] lg:h-[350px] rounded-none"/>
+    )
+}
+
+// Single blog skeleton
+export function SingleBlogSkeleton() {
+    return (
+        <div className="w-full flex-3 bg-transparent shadow-none">
+            <div className="flex flex-col gap-2">
+                <Skeleton className="bg-primary h-[260px] rounded-none block md:hidden"/>
+                <Skeleton className="w-full hidden md:flex gap-6 justify-between h-10 bg-accent rounded-none">
+                    <Skeleton className="flex-1 rounded-none"/>
+                    <Skeleton className="flex-6 rounded-none bg-accent"/>
+                </Skeleton>
+                <Skeleton className="w-full h-14 border-l-2 border-l-primary/50 bg-accent rounded-none"/>
+                <Skeleton className="w-full h-[400px] md:h-screen flex flex-col gap-10 p-5 bg-accent rounded-none"/>
+            </div>
+        </div>
+    )
+}
