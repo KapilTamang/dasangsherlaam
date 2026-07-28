74013460246945e35867a03ffb7f89a08dcb6d85
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-07-28 15:49:29 +0545

    feat: shadcn select component added to the project

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,30 +1,35 @@
 import Navbar from "@/components/shared/navbar"
 import Footer from "@/components/shared/footer"
-import { Button } from "@/components/ui/button"
 import { Field } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import blogs from "@/data/blogs"
 
 export default function Search() {
     return (
-        <>  <Navbar/>
+        <>  
+            <Navbar/>
             <main>
                 <section className="search-page-section section-base-style">
                     <div className="search-page-section-container container-base-style">
-                        <div className="search-page-section-content flex flex-col">
-                            <div className="search-page-section-input flex w-full justify-start">
-                             <Field className="w-[60%]" orientation="horizontal">
-                                <Input type="search" placeholder="Search..." />
-                                <Button>Search</Button>
-                            </Field>
-                        </div>
+                        <div className="search-page-section-content w-full flex flex-col gap-10">
+                            <div className="search-page-section-inputs flex gap-12 w-full items-center ">
+                                <div className="search-page-section-inputs-text flex flex-3">
+                                    <Field>
+                                        <Input id="input-button-group" placeholder="Type to search blogs.."/>
+                                    </Field>
+                                </div>
+                                <div className="search-page-section-inputs-select flex flex-1 items-center">
+                                    
+                                </div>
+                            </div>
+                            <div className="search-page-section-blogs">
+                                blogs
+                            </div>
                         </div>
                     </div>
                 </section>
-                <div className="section footer-section">
-                    <Footer/>
-                </div>
             </main>
+            <Footer/>
         </>
     )
 }
