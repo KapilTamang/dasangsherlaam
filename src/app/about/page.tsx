d6b5343ba1edeaa2ae818394335077d42a9c972e
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-07-28 13:03:35 +0545

    feat: about page completed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,42 +3,56 @@ import Image from "next/image";
 import { ModeToggle } from "@/components/shared/theme-toggle";
 import { House } from "lucide-react";
 import Author from '@/data/author';
+import { FaSquareFacebook, FaSquareInstagram, FaLinkedin, FaXTwitter} from "react-icons/fa6";
 
 export default function About() {
     return (
-        <main className="relative">
-            <div className="about-navigation absolute top-6 right-10 flex gap-6 items-center">
+        <main>
+            <div className="about-navigation fixed top-4 md:top-6 right-4 md:right-10 flex gap-6 items-center">
                 <Link href="/" className="flex gap-1 items-center text-[1.125rem] font-medium capitalize hover:opacity-50 duration-300 cursor-pointer"><House className="inline" size={20}/> home</Link>
                 <ModeToggle/>
             </div>
-            <article className="about section-base-style">
-                <div className="about-container container-base-style">
-                    <div className="about-content flex flex-col-reverse md:flex-row gap-8 justify-center items-center">
-                        <div className="about-content-left flex flex-2 flex-col gap-4">
-                            <div className="about-content-left-greetings">
-                                Hello! I'm {Author.name}
-                            </div>
-                            <div className="about-content-left-interoduction">
-                                I'm a {Author.title}
-                            </div>
-                            <div className="about-content-left-description">
-                                {Author.description}
-                            </div>
+            <article className="about">
+                <div className="about-content container-base-style fixed left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] flex flex-col-reverse md:flex-row gap-10 justify-center items-center">
+                    <div className="about-content-left flex flex-2 flex-col gap-8 md:gap-10 items-center md:items-start">
+                        <div className="about-content-left-greetings">
+                            <p className="text-[1.2rem] md:text-[1.4rem]">Hello! I'm <span className="border-l border-l-primary px-2 ml-1 md:ml-2 uppercase text-primary font-extrabold">{Author.name}</span></p> 
                         </div>
-                        <div className="about-content-right w-full flex flex-1 justify-center">
-                            <div className="about-content-right-image w-64 h-auto">
-                                <Image
-                                src={Author?.imageURL}
-                                width={1200}
-                                height={1000}
-                                alt={Author?.imageURL}
-                                preload
-                                />
-                            </div>
+                        <div className="about-content-left-interoduction mt-2">
+                            <p className="text-[1.4rem] md:text-[1.8rem] capitalize">I'm a <span className="uppercase font-extrabold bg-accent px-4 py-2 ml-1 md:ml-2 rounded-md">{Author.title}</span></p>
+                        </div>
+                        <div className="about-content-left-description">
+                            <p>{Author.description}</p>
+                        </div>
+                    </div>
+                    <div className="about-content-right flex flex-1 justify-center items-center">
+                        <div className="about-content-right-image w-50 md:w-64 h-auto">
+                            <Image
+                            className="rounded-sm shadow-xl"
+                            src={Author?.imageURL}
+                            width={1200}
+                            height={1000}
+                            alt={Author?.imageURL}
+                            preload
+                            />
                         </div>
                     </div>
                 </div>
             </article>
+            <div className="author-social-link fixed left-[50%] translate-x-[-50%] bottom-0 px-6 md:px-8 py-3 md:py-4 bg-accent flex gap-4 md:gap-5 rounded-full ">
+                <Link href="" className="hover:text-primary hover:scale-120 duration-300">
+                    <FaSquareFacebook size={28}/>
+                </Link>
+                <Link href="" className="hover:text-primary hover:scale-120 duration-300">
+                    <FaSquareInstagram size={28}/>
+                </Link>
+                <Link href="" className="hover:text-primary hover:scale-120 duration-300">
+                    <FaLinkedin size={28}/>
+                </Link>
+                <Link href="" className="hover:text-primary hover:scale-120 duration-300">
+                    <FaXTwitter size={28}/>
+                </Link>
+            </div>
         </main>
     )
 }
