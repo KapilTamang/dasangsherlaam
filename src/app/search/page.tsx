13e139481cfe7a5f948c2fb6c67f158ff0d1f08e
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-07-29 10:28:48 +0545

    feat: search functionality added to search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { ChangeEvent } from "react"
 import Navbar from "@/components/shared/navbar"
 import Footer from "@/components/shared/footer"
 import { Input } from "@/components/ui/input"
@@ -14,20 +14,93 @@ import NoData from "@/components/ui/no-data"
 
 export default function Search() {
     //Define loading state
-    const [isLoading, setIsLoading] = React.useState(true);
-
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+    //Define searcg loading state
+    const [isSearching, setIsSearching] = React.useState<boolean>(false);
     //Search result blogs
-    const [resultBlogs, setResultBlogs] = React.useState<Array<any>>([])
+    const [resultBlogs, setResultBlogs] = React.useState<Array<any>>([]);
+
+    //Define the structural shape of form state]
+    interface FormValues {
+        query: string;
+        category: string;
+    }
+
+    //Define state for form values
+    const [formValues, setFormValues] = React.useState<FormValues>({
+        query: '',
+        category: 'all'
+    });
 
     //Initial setup on page render
     React.useEffect(() => {
         const timer = setTimeout(() => {
-            const initialBlogs = blogs.sort(() => Math.random() - 0.5).slice(0,4);
-            setResultBlogs(initialBlogs)
+            //Get random blogs on page load
+            setResultBlogs(getRandomBLogs(blogs))
             setIsLoading(false);
         }, 2000);
        return() => clearTimeout(timer)
     }, []);
+
+    //Tracking input changes with useEffect
+    React.useEffect(() => {
+        setIsSearching(true)
+        const timer = setTimeout(() => {
+            //Filter blogs based on search query and category
+            const filtered = blogs.filter((blog) => {
+                //match input query with blogs title
+                const matchesQuery = blog.title
+                .toLowerCase()
+                .includes(formValues.query.toLowerCase());
+                //match input category with blog category
+                const matchesCategory = formValues.category === 'all' || formValues.category === blog.category;
+
+                return matchesQuery && matchesCategory;
+            });
+
+            //Check if searching reaches initial state of page loading
+            if(formValues.query === "")
+            {
+                setResultBlogs(getRandomBLogs(blogs));
+            }
+            else{
+                setResultBlogs(filtered);  
+            }
+            
+            //set search loading state
+            setIsSearching(false)
+        }, 2000)
+        return () => clearTimeout(timer);
+    }, [formValues]);
+
+    //helper function to filter random blogs for inital page loading state
+    const getRandomBLogs = (blogs: any[]) => {
+        return blogs.sort(() => Math.random()- 0.5).slice(0,4);
+    }
+
+    //Callback function to handle input change for search query
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormValues((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+        //set select input category value to default if search input is empty
+        if(value === "") {
+            setFormValues({
+                query: "",
+                category: "all"
+            })
+        }
+    };
+
+    //Callback function to hand select input change for category
+    const handleCategoryChange = (value: string) => {
+        setFormValues((prev) => ({
+            ...prev,
+            category: value
+        }));
+    };
 
     return (
         <>  
@@ -35,21 +108,21 @@ export default function Search() {
             <main>
                 <section className="search-page-section section-base-style">
                     <div className="search-page-section-container container-base-style">
-                        <div className="search-page-section-content w-full flex flex-col gap-10 md:gap-12 lg:gap-16">
+                        <div className="search-page-section-content w-full flex flex-col gap-8 md:gap-12 lg:gap-16">
                             <div className="search-page-section-inputs flex flex-col sm:flex-row gap-4 sm:gap-6 w-full items-center ">
                                 <div className="search-page-section-inputs-text relative w-full flex flex-1 sm:flex-2 lg:flex-3">
                                     <SearchIcon className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground"/>
-                                    <Input id="input-button-group" className="pl-9" placeholder="Search blogs..." disabled={isLoading}/>
+                                    <Input className="pl-9" placeholder="Search blogs..." name="query" value={formValues.query} onChange={handleInputChange} disabled={isLoading}/>
                                 </div>
                                 <div className="search-page-section-inputs-select w-full flex flex-1 items-center">
-                                    <Select defaultValue="all" disabled={isLoading}>
+                                    <Select name="category" value={formValues.category} onValueChange={handleCategoryChange} disabled={isLoading || formValues.query === ""}>
                                         <SelectTrigger className="w-full sm:w-50 md:w-64">
                                             <SelectValue/>
                                         </SelectTrigger>
                                         <SelectContent>
                                             <SelectGroup>
                                                 <SelectLabel>categories</SelectLabel>
-                                                <SelectItem value="all" defaultChecked>all categories</SelectItem>
+                                                <SelectItem value="all">all categories</SelectItem>
                                                 {
                                                     categories.map((category) => (
                                                         <SelectItem key={category.id} value={category.title}>
@@ -62,9 +135,9 @@ export default function Search() {
                                     </Select>
                                 </div>
                             </div>
-                            <div className="search-page-section-blogs flex flex-col gap-4">
+                            <div className="search-page-section-blogs flex flex-col gap-4 md:gap-6">
                                 {
-                                    isLoading ? 
+                                    isLoading || isSearching ? 
                                     (
                                         <div className="section-related-blogs-content-card w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-y-10">
                                             <CardColumnSkeleton cardNumber={4} cardType="grid"/>
@@ -73,13 +146,21 @@ export default function Search() {
                                     (
                                         resultBlogs && resultBlogs.length > 0 ? 
                                         (
-                                            <div className="section-related-blogs-content-card w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-y-10">
-                                                {
-                                                    resultBlogs.map((blog) => (
-                                                        <Card key={blog.id} data={blog} width="auto"/>
-                                                    ))
-                                                }
-                                            </div>
+                                            <>
+                                                <div className="flex justify-end items-center gap-2 text-[1rem] lg:text-[1rem] capitalize">
+                                                    searched results: 
+                                                    <span className="font-extrabold bg-accent px-2 py-0.5 rounded-sm">
+                                                        {resultBlogs.length}
+                                                    </span>
+                                                </div>
+                                                <div className="section-related-blogs-content-card w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-y-10">
+                                                    {
+                                                        resultBlogs.map((blog) => (
+                                                            <Card key={blog.id} data={blog} width="auto"/>
+                                                        ))
+                                                    }
+                                                </div>
+                                            </>
                                         ):
                                         (
                                             <NoData content="currently no blogs to show" orientation="horizontal"/>
