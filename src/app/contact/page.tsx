da23b540636a58fbb6a90946f89f1545474a8478
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-07-29 19:20:12 +0545

    feat: contact page basic layout created

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,34 @@
+import Image from "next/image"
+import NewsletterPromo from "@/components/shared/card/card-newsletter-promo"
+import Navbar from "@/components/shared/navbar"
+import Footer from "@/components/shared/footer"
+
+
 export default function Contact() {
     return (
-        <div className="global-padding">
+        <>
+            <Navbar/>
             <main>
-                This is Contact Page.
+                <section className="contact-page-section-banner section-base-style">
+                    <div className="contact-page-section-banner-container container-base-style">
+                       <div className="contact-page-section-banner-content flex w-full">
+                            <div className="w-full h-auto content-page-section-banner-content-image flex flex-1 justify-center items-center">
+                                <Image
+                                className=""
+                                src="/images/contact.png"
+                                alt="contact"
+                                width={900}
+                                height={900}
+                                preload/>
+                            </div>
+                            <div className="contact-page-section-banner-content-form flex w-full flex-1 justify-center items-center">
+                                <NewsletterPromo />
+                            </div>
+                       </div>
+                    </div>
+                </section>
             </main>
-        </div>
+            <Footer/>
+        </>
     )
 } 
