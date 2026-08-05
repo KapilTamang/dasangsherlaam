9400285b6f72d3af17a2dd086cdc40d4009f2bba
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-08-05 22:44:40 +0545

    feat: login page, schema and action file initiated for login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image"
+
 export default function Login() {
         return(
-            <div className="global-padding">
-                <main>
-                    This is Login Page.
-                </main>
-            </div>
+            <main>
+                <section className="login-page">
+                <Image
+                className="object-cover -z-10"
+                src="/images/login-bg.svg"
+                alt=""
+                preload
+                fill/>
+                <div className="login-form flex">
+                    login form
+                </div>
+                </section>
+            </main>
+            
         )
 }
