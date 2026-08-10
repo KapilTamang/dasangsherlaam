1dbf6636db97c4fd628c0e5e1b447c8f20ed8567
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-08-10 16:04:42 +0545

    feat: dashboard folder location changed to root folder

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function Error() {
             alt="error-bg-img"
             fill/>
             <div className="error-navigation fixed top-4 md:top-6 right-4 md:right-10 flex gap-6 items-center">
-                <Link href="/" className="flex gap-1 items-center text-[1rem] md:text-[1.1rem] font-medium capitalize hover:opacity-50 duration-300 cursor-pointer"><House className="inline" size={18}/> home</Link>
+                <Link href="/" className="flex gap-1 items-center text-[1rem] md:text-[1.1rem] text-black font-medium capitalize hover:opacity-50 duration-300 cursor-pointer"><House className="inline" size={18}/> home</Link>
             </div>
             <div className="error-text fixed top-[50%] left-[50%] translate-[-50%] flex w-[90%] md:max-w-96 gap-4 justify-center items-center bg-background text-foreground px-6 py-6 border-l-2 border-l-destructive shadow-xl">
                <div className="error-code text-[2rem] font-extrabold">
