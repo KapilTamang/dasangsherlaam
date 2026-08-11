969af25c2c38703b12a67a73df76d98af548f500
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-08-11 11:53:27 +0545

    feat: register page, register form file created

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import { RegisterForm } from "@/components/features/auth/register/register-form";
+import { ModeToggle } from "@/components/shared/theme-toggle";
+import { House } from "lucide-react";
+
+export default function Login() {
+        return( 
+            <main className="w-full h-screen bg-accent relative">
+                <Image
+                className="object-cover"
+                src="/images/login-bg.png"
+                alt="login-bg"
+                fill
+                preload/>
+                <div className="login-page">
+                    <div className="about-navigation fixed top-4 md:top-6 right-4 md:right-10 flex gap-4 md:gap-6 items-center">
+                        <Link href="/" className="flex gap-1 items-center text-[1rem] font-medium capitalize hover:opacity-50 duration-300 cursor-pointer"><House className="inline" size={18}/> home</Link>
+                        <ModeToggle/>
+                    </div>
+                    <div className="login-form fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full sm:w-120 px-6 md:px-0">
+                        <RegisterForm/>
+                    </div>
+                </div>
+            </main>
+            
+        )
+}
