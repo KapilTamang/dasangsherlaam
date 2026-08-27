f43d205acb03a32e4c576a241853bc53d6b058a8
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-08-27 23:09:01 +0545

    feat: adding time formatter utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatTime(seconds: number): string {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  
+  const paddedMins = String(mins).padStart(2, "0");
+  const paddedSecs = String(secs).padStart(2, "0");
+  
+  return `${paddedMins}:${secs.toString().padStart(2, "0")}`;
+}
