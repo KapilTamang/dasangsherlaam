1bf348d6bab3818e5145da8b7117a0b108f6c04c
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-08-29 16:58:06 +0545

    feat: action for forgot password form submission created

---
+++ src/actions/auth/forgot-password/action.ts
@@ -0,0 +1,34 @@
+import { ForgotPasswordFormValues, forgotPasswordSchema } from "./schema";
+
+export type ActionResponse = {
+    success: boolean;
+    message: string;
+}
+
+export async function submitForgotPasswordForm(data: ForgotPasswordFormValues): Promise<ActionResponse> {
+    // Re-validate incoming client payloads on the server
+    const validateFields = forgotPasswordSchema.safeParse(data);
+
+    if(!validateFields) {
+        return {
+            success: false,
+            message: "Invalid form data."
+        }
+    }
+    else{
+        //Process the valid data from here
+        try {
+            //Process your database or API call here
+            return {
+                success: true,
+                message: "Request sent. Please check your email for verification code."
+            }
+        }
+        catch(error) {
+            return {
+                success: false,
+                message: "An error occured while processing the form."
+            }
+        }
+    }
+} 
