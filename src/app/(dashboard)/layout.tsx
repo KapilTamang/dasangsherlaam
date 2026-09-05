ff247122c9e5f37f400528e687117021e492faa0
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-09-05 13:23:46 +0545

    feat: top nav created for dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,7 @@
 // app/(dashboard)/layout.tsx
-import {SidebarTrigger, SidebarProvider, SidebarInset} from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset} from "@/components/ui/sidebar";
 import { AppSidebar } from "./dashboard/_components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import {Separator} from "@/components/ui/separator"
+import TopNav from "./dashboard/_components/top-nav";
 
 export default function DashboardLayout({
   children,
@@ -20,28 +12,7 @@ export default function DashboardLayout({
     <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-				<div className="flex items-center gap-2 px-4">
-					<SidebarTrigger className="-ml-1" />
-					<Separator
-					orientation="vertical"
-					className="mr-2 data-[orientation=vertical]:h-4"
-					/>
-					<Breadcrumb>
-						<BreadcrumbList>
-							<BreadcrumbItem className="hidden md:block">
-								<BreadcrumbLink href="#">
-									Build Your Application
-								</BreadcrumbLink>
-							</BreadcrumbItem>
-							<BreadcrumbSeparator className="hidden md:block" />
-							<BreadcrumbItem>
-								<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-							</BreadcrumbItem>
-						</BreadcrumbList>
-					</Breadcrumb>
-				</div>
-			</header>
+			<TopNav/>
 			<main>
 				{children}
 			</main>
