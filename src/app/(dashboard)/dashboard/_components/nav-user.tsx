9f423308e540e0cdf40e27cdc060a04ff71834d4
Kapil Tamang <tamang.kapil11235@gmail.com>
2026-09-05 14:00:55 +0545

    fix: fixing top nav breadcum separator alignment

--- src/app/(dashboard)/dashboard/_components/nav-user.tsx
+++ src/app/(dashboard)/dashboard/_components/nav-user.tsx
@@ -65,7 +65,7 @@ export function NavUser() {
 						<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 							<Avatar className="h-8 w-8 rounded-lg">
 							<AvatarImage src={author.imageURL} alt={author.name} />
-							<AvatarFallback className="rounded-lg">CN</AvatarFallback>
+							<AvatarFallback className="rounded-lg">D</AvatarFallback>
 							</Avatar>
 							<div className="grid flex-1 text-left text-sm leading-tight">
 							<span className="truncate font-medium">{author.name}</span>
--- src/app/(dashboard)/dashboard/_components/top-nav.tsx
+++ src/app/(dashboard)/dashboard/_components/top-nav.tsx
@@ -15,12 +15,12 @@ import { ModeToggle } from "@/components/shared/theme-toggle"
 
 export default function TopNav () {
     return(
-        <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 shadow-sm">
+        <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 shadow-xs">
             <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator
                 orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
+                className="mr-2 data-[orientation=vertical]:h-4 my-auto"
                 />
                 <Breadcrumb>
                     <BreadcrumbList>
